ac412eb8f6c56fe56a789e862a33a52f92a22b0f
Darren Wang <wangd4@bxscience.edu>
2019-08-10 13:56:03 -0400

    Delete popup.js

--- js/popup.js
+++
@@ -1,5 +0,0 @@
-//Makes the popup page accordingly to the href stored in the button
-$('[id^="a"]').click(function(){
-	var page = 'html/' + $(this).attr('href');
-	chrome.browserAction.setPopup({popup: page});
-});
